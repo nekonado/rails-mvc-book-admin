975e019f3036e2bd697fe6ce7e786dbd8cade158
queuek <lahass1213@gmail.com>
2023-10-01 15:15:04 +0900

    add relational table to books

---
+++ db/migrate/20231001053037_add_publisher_id_books.rb
@@ -0,0 +1,6 @@
+class AddPublisherIdBooks < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :books, :publisher, foreign_key: true
+    change_column :books, :publisher_id, :integer, null: false
+  end
+end
