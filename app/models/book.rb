b3dba7b165f6298d736d7ba5ff03e9d28de888d6
queuek <lahass1213@gmail.com>
2023-10-02 00:26:44 +0900

    add validation to book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,14 @@ class Book < ApplicationRecord
   belongs_to :publisher
   has_many :book_authors
   has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  # 以下のカスタムのバリデーションがうまく機能していないが理由がよくわからない
+  validate do |book|
+    if book.name.include?("cat")
+      book.errors[:name] << "I love cat so much."
+    end
+  end
 end
